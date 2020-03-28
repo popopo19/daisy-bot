02584269f2c30eba7c24ab736f10a89e550d44fb
popopo19 <syan317@gmail.com>
2020-03-28 13:35:09 -0500

    fix-test #0

--- events/message.js
+++ events/message.js
@@ -3,9 +3,12 @@ module.exports = {
 }
 
 function eventHandler(client, msg) {
+  console.log("Function has been called")
   if (msg.content === 'ping') {
+    console.log("Pong")
     return msg.reply('pong no life');
   } else if (msg.content === "!name") {
+    console.log("Name")
     return msg.channel.send(msg.author.username);
   }
 }
