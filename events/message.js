8f1b1f71c7df1e6e9a281afdc65725d5e2f6ded9
c0pper105 <46850493+c0pper105@users.noreply.github.com>
2020-03-28 18:45:52 -0500

    chagnes to rps

--- events/message.js
+++ events/message.js
@@ -20,7 +20,7 @@ module.exports = (client, msg) => {
 // !rps
   } else if (words[0] === "!rps") {
     msg.reply('Choose rock, paper, or scissors.');
-  } else if (words[0] === "rock") {
+  } else if mgs.author.username === "rock") {
     msg.reply('You lose');
 
 // User didn't type in right command
