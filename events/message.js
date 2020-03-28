d79aa133d739b23ff050c1913ef6cfabd8e70262
popopo19 <syan317@gmail.com>
2020-03-28 15:30:30 -0500

    Adding Feature: "!random"

--- events/message.js
+++ events/message.js
@@ -1,8 +1,16 @@
 module.exports = (client, msg) => {
   let words = msg.content.split(" ")
-  if (words[0] === 'ping') {
+  if (msg.content === 'ping') {
     msg.reply('pong no life');
   } else if (words[0] === "!name") {
     return msg.channel.send(msg.author.username);
+  } else if (words[0] === "!random") {
+    let len = words.length
+    if (!isNaN(words[1]) || len == 2) {
+      let rand = Math.floor(Math.random() * words[1])
+      msg.reply(rand)
+    } else {
+      msg.reply("!random [number]")
+    }
   }
 }
