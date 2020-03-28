6b61d89dd2a9f66d99901a4fdf0d18800519717a
popopo19 <syan317@gmail.com>
2020-03-28 15:33:35 -0500

    Changing Feature: "!random" to "!dice"

--- events/message.js
+++ events/message.js
@@ -4,13 +4,13 @@ module.exports = (client, msg) => {
     msg.reply('pong no life');
   } else if (words[0] === "!name") {
     return msg.channel.send(msg.author.username);
-  } else if (words[0] === "!random") {
+  } else if (words[0] === "!dice") {
     let len = words.length
     if (!isNaN(words[1]) || len == 2) {
       let rand = Math.floor(Math.random() * words[1])
       msg.reply(rand)
     } else {
-      msg.reply("!random [number]")
+      msg.reply("!dice [number]")
     }
   }
 }
