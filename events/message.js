efafa5a2f386802624719126a975b2105df1bc71
popopo19 <syan317@gmail.com>
2020-03-27 21:05:11 -0500

    Fixed

--- events/message.js
+++ events/message.js
@@ -1,7 +1,7 @@
 module.exports = (client, msg) => {
   if (msg.content === 'ping') {
-    msg.reply('pong no life');
+    return msg.reply('pong no life');
   } else if (msg.content === "!name") {
-    msg.channel.send(msg.author.username);
+    return msg.channel.send(msg.author.username);
   }
 }
