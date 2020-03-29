741b49b05978ea4fb57643f633dc5fcfb7510141
popopo19 <syan317@gmail.com>
2020-03-29 16:59:51 -0500

    added specific option for new riddle

--- commands/riddle.js
+++ commands/riddle.js
@@ -2,15 +2,18 @@ module.exports = (client, msg) => {
   const cmd = require("../commands/cmd.json")
   let words = msg.content.split(" ")
 
-  if (cmd.riddle.current == null || words.length == 1) {
+  // If no riddle is asked of yet
+  if (cmd.riddle.current == null || words[1] == "-n") {
     let rand = Math.floor(Math.random() * cmd.riddle.questions.length)
 
     cmd.riddle.current = rand
-    msg.channel.send(cmd.riddle.questions[rand] + "\nTo answer: !riddle [answer]")
+    msg.channel.send(cmd.riddle.questions[rand] + "\nTo answer: !riddle [answer] OR\n for new riddle: !riddle -n")
+  // If riddle is already asked, check for correctness
   } else {
 
     let correct = false
 
+    // Checks if user answer is correct
     for (let i = 1; i < words.length; i++) {
       for (let j = 0; j < cmd.riddle.answers[cmd.riddle.current].length; j++) {
         if (words[i].toLowerCase() == cmd.riddle.answers[cmd.riddle.current][j].toLowerCase()) {
