cfbd7da707587b17822a8f5993bd9ac38e778e96
popopo19 <syan317@gmail.com>
2020-03-29 14:48:50 -0500

    random picking riddles

--- commands/dice.js
+++ commands/dice.js
@@ -2,7 +2,7 @@ module.exports = (msg, words) => {
   let len = words.length
 
   if (!isNaN(words[1]) && len == 2) { // Makes sure msg is in correct format
-    let rand = Math.floor(Math.random() * words[1]) + 1
+    let rand = Math.ceil(Math.random() * words[1])
     msg.reply(rand)
   } else {
     msg.reply("!dice [number]")
--- commands/riddle.js
+++ commands/riddle.js
@@ -1,3 +1,6 @@
 module.exports = (client, msg) => {
-  
+  const cmd = require("../commands/cmd.json")
+
+  let rand = Math.floor(Math.random() * (cmd.riddle.length / 2))
+  msg.channel.send(cmd.riddle[rand * 2])
 }
