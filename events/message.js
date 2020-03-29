cca9fccf07b117ed7edf3a3a2cb164be3bbaceaa
popopo19 <syan317@gmail.com>
2020-03-29 14:14:30 -0500

    trying to add !riddle

--- events/message.js
+++ events/message.js
@@ -1,31 +1,39 @@
 module.exports = (client, msg) => {
   let words = msg.content.split(" ")
 
-// ping
-  if (msg.content.toLowerCase() === 'ping') {
-    msg.reply('pong no life');
-  } else if (words[0] === "!name") {
-    return msg.channel.send(msg.author.username);
+  // commands
+  if (words[0][0] == '!') {
 
-// !dice
-  } else if (words[0] === "!dice") {
-    const dice = require("../commands/dice.js")
-    dice(msg, words)
+    // !dice
+    if (words[0] === "!dice") {
+      const dice = require("../commands/dice.js")
+      dice(msg, words)
 
-// !rps
-  } else if (words[0] === "!rps") {
-    // let player_bot = random('rock','paper','scissor');
-  } else if (words[0] === "rock" && player_bot === 'rock') {
-    msg.reply('Tie');
+    // !rps
+    } else if (words[0] === "!rps") {
+      // let player_bot = random('rock','paper','scissor');
+    } else if (words[0] === "rock" && player_bot === 'rock') {
+      msg.reply('Tie');
 
-// User didn't type in right command
-} else if (words[0] == '!help') {
-  const help = require("../commands/help.js")
-  help(msg)
 
-// !help
-} else if (words[0][0] == '!') {
-  const wrong_cmd = require("../commands/wrong_cmd.js")
-  wrong_cmd(msg)
+    } else if (wrods[0] === '!riddle') {
+      const riddle = require("../commands/riddle.js")
+      riddle(client, msg)
+
+    // !help
+    } else if (words[0] == '!help') {
+      const help = require("../commands/help.js")
+      help(msg)
+
+    // User didn't type in right command
+    } else {
+      const wrong_cmd = require("../commands/wrong_cmd.js")
+      wrong_cmd(msg)
+    }
+  } else {
+    // ping
+    if (msg.content.toLowerCase() === 'ping') {
+      msg.reply('pong');
+    }
   }
 }
