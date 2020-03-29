1814d79cb5a1b9450d8af3777895b292787fcb0d
popopo19 <syan317@gmail.com>
2020-03-29 17:38:23 -0500

    updated !help

--- commands/help.js
+++ commands/help.js
@@ -3,8 +3,8 @@ module.exports = msg => {
   let reply_msg = "\nList of commands\n"
 
   // Go through list in cmd_list.json
-  for (let i = 0; i < cmd.list.length; i++) {
-    reply_msg += cmd.list[i] + "\t"
+  for (let i = 0; i < cmd.help.list.length; i++) {
+    reply_msg += cmd.help.list[i] + "\t"
   }
 
   msg.reply(reply_msg)
