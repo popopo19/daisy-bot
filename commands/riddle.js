d9c0c6890bedfd11f2ebc88f99c114278db25f9c
popopo19 <syan317@gmail.com>
2020-03-29 16:11:01 -0500

    developed !riddle

--- commands/riddle.js
+++ commands/riddle.js
@@ -1,6 +1,28 @@
 module.exports = (client, msg) => {
   const cmd = require("../commands/cmd.json")
+  let words = msg.content.split(" ")
 
-  let rand = Math.floor(Math.random() * (cmd.riddle.length / 2))
-  msg.channel.send(cmd.riddle[rand * 2])
+  if (riddle.current == null || words.length == 1) {
+    let rand = Math.floor(Math.random() * cmd.riddle.questions.length)
+
+    cmd.riddle.current = rand
+    msg.channel.send(cmd.riddle.questions[rand])
+  } else {
+
+    let correct = false
+
+    for (let i = 1; i < words.length; i++) {
+      for (let j = 0; j < cmd.riddle.answers.length; j++) {
+        if (words[i] == cmd.riddle.answers[j]) {
+          correct = true
+        }
+      }
+    }
+    if (correct == true) {
+      cmd.riddle.current = null
+      msg.reply("You got the right answer")
+    } else {
+      msg.reply("You got the wrong answer")
+    }
+  }
 }
