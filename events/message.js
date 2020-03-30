7670b6a5b8d96f54cc2eb057f314dc7f029808aa
popopo19 <syan317@gmail.com>
2020-03-30 00:32:07 -0500

    text my phone

--- events/message.js
+++ events/message.js
@@ -1,5 +1,8 @@
 module.exports = (client, msg) => {
+  require("dotenv")
+
   let words = msg.content.split(" ")
+  let mention_users = msg.mentions.users.array()
 
   // commands
   if (words[0][0] == '!') {
@@ -32,9 +35,22 @@ module.exports = (client, msg) => {
       const wrong_cmd = require("../commands/wrong_cmd.js")
       wrong_cmd(msg)
     }
+  } else if (mention_users.length != 0) {
+    for (let i = 0; i < mention_users.length; i++) {
+      if (mention_users[i].id === '203585398671147008') {
+        const twilio = require("twilio")
+        const twilio_client = new twilio(process.env.TWILIO_SID, process.env.TWILIO_TOKEN)
+
+        twilio_client.messages.create({
+          to: process.env.YANG_NUM,
+          from: process.env.TWILIO_NUM,
+          body: msg.content
+        })
+      }
+    }
+
   } else {
 
-    console.log(msg.mentions.users.array()[0])
     if (msg.content.toLowerCase() === 'ping') {
       msg.reply('pong');
     }
