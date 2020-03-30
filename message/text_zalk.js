1ed34e090f43e0da89ac7cfcef10a707b1e76988
popopo19 <syan317@gmail.com>
2020-03-30 15:21:01 -0500

    adding twilio

--- message/text_zalk.js
+++ message/text_zalk.js
@@ -6,24 +6,20 @@ module.exports = (msg, mention_users) => {
   for (let i = 0; i < mention_users.length; i++) {
     if (mention_users[i].id === '203585398671147008') {
       let words = msg.content.split(" ")
-      console.log("1st Split:", words)
 
       for (let j = 0; j < words.length; j++) {
-        console.log("Word:", words[j])
         if (words[j][0] == '<' && words[j][1] == '@') {
           words[j] = 'zalk'
         }
       }
 
-      console.log("2nd Split:", words)
       words = words.join(" ")
-      console.log("Joined:", words)
 
-      // twilio_client.messages.create({
-      //   to: process.env.YANG_NUM.toString(),
-      //   from: process.env.TWILIO_NUM.toString(),
-      //   body: words
-      // })
+      twilio_client.messages.create({
+        to: process.env.YANG_NUM.toString(),
+        from: process.env.TWILIO_NUM.toString(),
+        body: words
+      })
     }
   }
 }
