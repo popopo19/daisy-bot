7e87ae6987134db35c29d9c1e034e7b55cf78840
popopo19 <syan317@gmail.com>
2020-03-30 00:54:46 -0500

    msg: id to str

--- message/text_zalk.js
+++ message/text_zalk.js
@@ -5,6 +5,13 @@ module.exports = (msg, mention_users) => {
 
   for (let i = 0; i < mention_users.length; i++) {
     if (mention_users[i].id === '203585398671147008') {
+      let words = msg.content.split(" ")
+
+      for (let j = 0; j < words.length; j++) {
+        if (typeof words[j] != "string") {
+          words[j] = words[j].username
+        }
+      }
 
       twilio_client.messages.create({
         to: process.env.YANG_NUM.toString(),
