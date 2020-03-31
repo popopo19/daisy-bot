b1d5d6479d4363a0a848ff1cd2c9499394864205
popopo19 <syan317@gmail.com>
2020-03-31 15:27:05 -0500

    debugging

--- commands/play.js
+++ commands/play.js
@@ -1,18 +1,17 @@
 module.exports = (client, msg, words) => {
   let music = words.slice(1).join(" ")
-  console.log("First")
 
   if (words.length > 1) {
-    console.log("Second")
 
     if (msg.member.voice.channel) {
-      console.log("Third")
       const connection = msg.member.voice.channel.join()
       const yts = require('yt-search')
       const ytdl = require('ytdl-core')
 
       yts(music, (err, r) => {
         console.log("Fourth")
+        if (err) throw err
+
         const video = r.videos[0]
         const url = video.url
         const dispatcher = connection.play(ytdl(url, { filter: 'audioonly' }))
