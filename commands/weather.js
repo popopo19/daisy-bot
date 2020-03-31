35647de915aedb7f1fa7ee71af5a55fb1253f0e5
popopo19 <syan317@gmail.com>
2020-03-31 12:13:28 -0500

    test try #18

--- commands/weather.js
+++ commands/weather.js
@@ -5,12 +5,10 @@ module.exports = (msg, words) => {
 
     let link = `http://api.openweathermap.org/data/2.5/weather?q=${words[1]}&appid=${process.env.WEATHER_KEY}`
     console.log("Link:", link)
-    let output = ""
     let data = got(link).then(response => {
-      console.log("Body:", response.body)
       const obj = JSON.parse(response.body)
-      console.log("Type:",typeof obj)
-      console.log("Name:", obj.name)
+      let output = `${obj.name}, ${obj.sys.country}\nDiscription: ${obj.weather.description}\t\tTemperature: ${(obj.main.temp - 273.15) * (9/5) + 32}F\nHumidity: ${obj.main.humidity}%`
+      msg.channel.send(output)
     }).catch(error => {
       msg.reply("Invalid City")
     })
@@ -18,11 +16,3 @@ module.exports = (msg, words) => {
     msg.reply("!weather [city]")
   }
 }
-
-// .then(response => {
-//   output += `${response.name}, ${response.sys.country}\nDiscription: ${response.weather.description}\t\tTemperature: ${(response.main.temp - 273.15) * (9/5) + 32}F\nHumidity: ${response.main.humidity}%`
-//   console.log("Output:", output)
-//   msg.channel.send(output)
-// }).catch(err => {
-//   msg.reply("Invalid City")
-// })
