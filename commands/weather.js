5463d331a329af6a005e5b75ee974ac3da6c9371
popopo19 <syan317@gmail.com>
2020-03-31 10:59:43 -0500

    debugging !weather

--- commands/weather.js
+++ commands/weather.js
@@ -4,9 +4,11 @@ module.exports = (msg, words) => {
     const got = require('got')
 
     let link = `http://api.openweathermap.org/data/2.5/weather?q=${words[1]}&appid=${process.env.WEATHER_KEY}`
+    console.log("Link:", link)
     let output = ""
     let data = got(link, {json:true}).then(response => {
       output += `${response.name}, ${response.sys.country}\nDiscription: ${response.weather.description}\t\tTemperature: ${(response.main.temp - 273.15) * (9/5) + 32}F\nHumidity: ${response.main.humidity}%`
+      console.log("Output:", output)
       msg.channel.send(output)
     }).catch(err => {
       msg.reply("Invalid City")
