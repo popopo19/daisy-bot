3155e3604b0b3ea1b9a83e68681b87ead7a5a97a
popopo19 <syan317@gmail.com>
2020-03-31 15:24:20 -0500

    debugging semantic error

--- commands/play.js
+++ commands/play.js
@@ -1,18 +1,20 @@
 module.exports = (client, msg, words) => {
   let music = words.slice(1).join(" ")
+  console.log("First")
 
   if (words.length > 1) {
+    console.log("Second")
 
     if (msg.member.voice.channel) {
+      console.log("Third")
       const connection = msg.member.voice.channel.join()
       const yts = require('yt-search')
       const ytdl = require('ytdl-core')
 
       yts(music, (err, r) => {
-        console.log("R:",r)
+        console.log("Fourth")
         const video = r.videos[0]
         const url = video.url
-        console.log("URL:", url)
         const dispatcher = connection.play(ytdl(url, { filter: 'audioonly' }))
       })
     } else {
