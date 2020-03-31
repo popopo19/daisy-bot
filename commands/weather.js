edc99083d6f6793d55f432f74ab036c6b902cd53
popopo19 <syan317@gmail.com>
2020-03-31 12:29:39 -0500

    preventing input city error scenerios

--- commands/weather.js
+++ commands/weather.js
@@ -1,13 +1,19 @@
 module.exports = (msg, words) => {
 
-  if (words.length == 2) {
+  if (words.length >= 2) {
+    let name = words.slice(1).join(" ")
+    console.log("Name:", name)
+
     const got = require('got')
 
-    let link = `http://api.openweathermap.org/data/2.5/weather?q=${words[1]}&appid=${process.env.WEATHER_KEY}`
-    console.log("Link:", link)
+    let link = `http://api.openweathermap.org/data/2.5/weather?q=${name}&appid=${process.env.WEATHER_KEY}`
+
+    // Get data from api website
     let data = got(link).then(response => {
+      // Change data from string to object
       const obj = JSON.parse(response.body)
       let output = `${obj.name}, ${obj.sys.country}\nDiscription: ${obj.weather[0].description}\t\tTemperature: ${((obj.main.temp - 273.15) * (9/5) + 32).toFixed(2)}F\nHumidity: ${obj.main.humidity}%`
+
       msg.channel.send(output)
     }).catch(error => {
       msg.reply("Invalid City")
