79759646d84cdc0f87ee38373b082dcbb7f566b0
popopo19 <syan317@gmail.com>
2020-03-31 10:56:10 -0500

    adding !weather

--- commands/weather.js
+++ commands/weather.js
@@ -1,5 +1,17 @@
 module.exports = (msg, words) => {
-  const got = require('got')
 
-  
+  if (words.length == 2) {
+    const got = require('got')
+
+    let link = `http://api.openweathermap.org/data/2.5/weather?q=${words[1]}&appid=${process.env.WEATHER_KEY}`
+    let output = ""
+    let data = got(link, {json:true}).then(response => {
+      output += `${response.name}, ${response.sys.country}\nDiscription: ${response.weather.description}\t\tTemperature: ${(response.main.temp - 273.15) * (9/5) + 32}F\nHumidity: ${response.main.humidity}%`
+      msg.channel.send(output)
+    }).catch(err => {
+      msg.reply("Invalid City")
+    })
+  } else {
+    msg.reply("!weather [city]")
+  }
 }
