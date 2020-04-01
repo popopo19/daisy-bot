ecad6d945fb7f2f07cfcbce887b1d29f431250a5
popopo19 <syan317@gmail.com>
2020-04-01 12:31:35 -0500

    debugging #9

--- commands/play.js
+++ commands/play.js
@@ -20,7 +20,7 @@ module.exports = (client, msg, words) => {
       // })
 
       msg.member.voice.channel.join()
-      const connection = client.voice.connections.array
+      const connection = client.voice.connections.array()
       console.log(connection)
       const dispatcher = connection.play(ytdl(url, { filter: 'audioonly' }))
 
