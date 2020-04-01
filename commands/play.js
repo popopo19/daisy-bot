0e136773c72193f8bfe95088cf0945bb38f982c7
popopo19 <syan317@gmail.com>
2020-04-01 11:58:16 -0500

    trying youtube-search api

--- commands/play.js
+++ commands/play.js
@@ -5,22 +5,28 @@ module.exports = (client, msg, words) => {
   if (words.length > 1) {
 
     if (msg.member.voice.channel) {
-      const yts = require('yt-search')
+      // const yts = require('yt-search')
+      const search = require('youtube-search')
       const ytdl = require('ytdl-core')
       const url = 'https://www.youtube.com/watch?v=KRcj6gkfx4c'
-      let video = null
+      const opts = {
+        maxResults: 1,
+        key: process.env.YT_KEY
+      }
 
-      yts(music, (err, r) => {
-        console.log("Once")
-
-        video = r.videos[0]
-        console.log("First", video)
+      search(music, opts, (err, r) => {
+        console.log(r)
       })
 
-      console.log("Second", video)
-      url = video.url
-      const connection = msg.member.voice.channel.join()
-      const dispatcher = connection.play(ytdl(url, { filter: 'audioonly' }))
+      // yts(music, (err, r) => {
+      //   console.log("Once")
+      //
+      //   video = r.videos[0]
+      //   console.log("First", video)
+      // })
+
+      // const connection = msg.member.voice.channel.join()
+      // const dispatcher = connection.play(ytdl(url, { filter: 'audioonly' }))
 
     } else {
       msg.reply("Join a voice channel first")
