650aa2a64e045ebcec97697ff302750812369e36
popopo19 <syan317@gmail.com>
2020-04-14 23:45:00 -0500

    stage 2: created class for poll

--- commands/poll.js
+++ commands/poll.js
@@ -13,28 +13,70 @@ module.exports = (client, msg, word) => {
     }
 
   } else if (list[0] != '') {
-    // console.log("Does functions")
-    cmd.poll.items = []
-    cmd.poll.votes = []
-    for (let i = 0; i < list.length && list[i] != ''; i++) {
-      cmd.poll.items.push(list[i])
-      cmd.poll.votes.push(0)
-    }
+    console.log("Does functions")
+    // cmd.poll.items = []
+    // cmd.poll.votes = []
+    // for (let i = 0; i < list.length && list[i] != ''; i++) {
+    //   cmd.poll.items.push(list[i])
+    //   cmd.poll.votes.push(0)
+    // }
+    //
+    // msg.channel.send(sendPoll(cmd.poll.items, cmd.poll.emojis))
+    //
+    // cmd.poll.pollCount = cmd.poll.items.length
+    // cmd.poll.using = true
 
-    msg.channel.send(sendPoll(cmd.poll.items, cmd.poll.emojis))
 
-    cmd.poll.pollCount = cmd.poll.items.length
-    cmd.poll.using = true
 
   } else {
     msg.reply("!poll [OPTION] [item1], [item2], ...")
   }
 }
 
-function sendPoll(items, emotes) {
-  let output = ""
-  for (let i = 0; i < items.length; i++) {
-    output += `${emotes[i]} ${items[i]}\t\t `
+class Poll {
+  constructor(id, items, votes, emojis) {
+    this.id = id
+    this.items = items
+    this.votes = votes
+    this.emojis = emojis
   }
-  return output
+
+  get getId() {return this.id }
+  get getItems() { return this.items }
+  get getVotes() { return this.votes }
+
+  function sendPoll(items, emotes) {
+    let output = ""
+    for (let i = 0; i < items.length; i++) {
+      output += `${emotes[i]} ${items[i]}\t\t `
+    }
+    return output
+  }
+
+  function announceHighest() {
+    let highest = 0
+    let indexOfHighest = 0
+    for (let i = 0; i < this.votes.length; i++) {
+      if (this.votes[i] > highest) {
+        highest = this.votes[i]
+        indexOfHighest = i
+      }
+    }
+
+    return `The most voted goes to ${this.items[indexOfHighest]} with ${highest} votes.`
+  }
+
+  function announceLowest() {
+    let lowest = 0
+    let indexOfLowest = 0
+    for (let i = 0; i < this.votes.length; i++) {
+      if (this.votes[i] < lowest) {
+        lowest = this.votes[i]
+        indexOfLowest = i
+      }
+    }
+
+    return `The unpopular vote goes to ${this.items[indexOfLowest]} with ${lowest} votes.`
+  }
+
 }
